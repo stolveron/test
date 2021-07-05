f80886f7d5608c239132dc500ea38e0ae5a66fd5
stolveron <natasinuk@mail.ru>
2021-07-05 18:54:41 +0300

    Реализован запуск калькулятора через test приложение

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setComponent(new ComponentName("com.example.calculate_1","com.example.calculate_1.MainActivity"));
+        startActivity(intent);
+
     }
+
 }
